b84800449007600dc7db83d86f2865ebaf854a7a
Emily Hodges <ehodges9@gatech.edu>
2020-12-01 02:34:55 -0500

    Redoing  scales, working on plotting data

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ var chart1_cy = 400;
 
 
 d3.csv("Spotify-2000.csv", function (csv) {
-  console.log(csv);
   for (var i = 0; i < csv.length; ++i) {
     csv[i].bpm = Number(csv[i]["Beats Per Minute (BPM)"]);
-	  csv[i].Loudness = Number(csv[i]["Loudness (dB)"]);
+	  csv[i].Loudness = 28 + Number(csv[i]["Loudness (dB)"]);
 	  csv[i].Length = Number(csv[i]["Length (Duration)"]);
     csv[i].Danceability = Number(csv[i].Danceability);
     csv[i].Energy = Number(csv[i].Energy);
@@ -24,6 +23,14 @@ d3.csv("Spotify-2000.csv", function (csv) {
 
   // Finding the extent of each value [min,max]
 
+  function get_extent(attr) {
+    return d3.extent(csv, function(row) {
+      return row[attr];
+    })
+  }
+
+
+
   var bpmExtent = d3.extent(csv, function(row) {
     return row.bpm;
   });
@@ -31,6 +38,7 @@ d3.csv("Spotify-2000.csv", function (csv) {
   var loudExtent = d3.extent(csv, function(row) {
     return row.Loudness;
   });
+
 
   var lengthExtent = d3.extent(csv, function(row) {
     return row.Length;
@@ -58,66 +66,67 @@ d3.csv("Spotify-2000.csv", function (csv) {
 
   var speechExtent = d3.extent(csv, function(row) {
     return row.Speechiness;
-  }); 
+  });  
 
   // --------------------------- SCALE SETUP -----------------------------
 
-  // Radial scale for circles
-  var domainMin = d3.min([danceExtent[0], energyExtent[0], liveExtent[0], valenceExtent[0],
-    acousticExtent[0], speechExtent[0]]);
   
-  var domainMax = d3.min([danceExtent[1], energyExtent[1], liveExtent[1], valenceExtent[1],
-    acousticExtent[1], speechExtent[1]]);
-
-    console.log("Domain min: " + domainMax);
+  var domainMax = d3.max([bpmExtent[1], loudExtent[1], lengthExtent[1], danceExtent[1], 
+    energyExtent[1], liveExtent[1], valenceExtent[1], acousticExtent[1], speechExtent[1]]);
 
   var radialScale = d3.scaleLinear()
-    .domain([domainMin, domainMax])
+    .domain([0, domainMax])
     .range([0,rangeMax]);
 
 
+
   // Scales for the different attribute axes
-  var bpmScale = d3.scaleLinear().domain(bpmExtent).range([0,rangeMax]);
-  var loudScale = d3.scaleLinear().domain(loudExtent).range([0,rangeMax]);
-  var lengthScale = d3.scaleLinear().domain(lengthExtent).range([0,rangeMax]);
-  var danceScale = d3.scaleLinear().domain(danceExtent).range([0,rangeMax]);
-  var energyScale = d3.scaleLinear().domain(energyExtent).range([0,rangeMax]);
-  var liveScale = d3.scaleLinear().domain(liveExtent).range([0,rangeMax]);
-  var valenceScale = d3.scaleLinear().domain(valenceExtent).range([0,rangeMax]);
-  var acousticScale = d3.scaleLinear().domain(acousticExtent).range([0,rangeMax]);
-  var speechScale = d3.scaleLinear().domain(speechExtent).range([0,rangeMax]);
+  function scale(attr) {
+    if (attr == "radial") {
+      return d3.scaleLinear().domain([0, domainMax]).range([0, rangeMax]);
+    } 
+      let extent = get_extent(attr);
+      return d3.scaleLinear().domain(extent).range([0, rangeMax]);
+  }
 
   // ----------------------------- AXES SETUP ----------------------------
 
   // Gets the coordinate on the x-y plane of the given value at a given angle
   // returned as an object with attributes "x" and "y"
-  function getCoordinate(angle, value) {
-    let x = Math.cos(angle) * radialScale(value);
-    let y = Math.sin(angle) * radialScale(value);
-    return {"x": chart1_cx + x, "y": chart1_cy - y};
+  function getCoordinate(angle, value, attr) {
+    // ???????????????????????????????????????????????????????????????????????????????????????
+    // ERROR: I have no clue why the scale function isn't working for attr,
+    // I think it has something to do with passing in the string as a variable, but
+    // scale(attr)(value) is returning NaN 
+    // ???????????????????????????????????????????????????????????????????????????????????????
+    let x = Math.cos(angle) * scale(attr)(value);
+    let y = Math.sin(angle) * scale(attr)(value);
+
+    return [chart1_cx + x, chart1_cy - y];
   }
 
-  var attributes = ["Beats Per Minute", "Loudness", "Length", "Danceability", "Energy", "Liveness", "Valence", "Acousticness", "Speechiness"];
+  var attributes = ["Beats Per Minute (BPM)", "Loudness (dB)", "Length (Duration)", "Danceability", "Energy", "Liveness", "Valence", "Acousticness", "Speechiness"];
+  
   function drawAxes(labels, chart) {
-    let lineWidth = (numRings * 2) + 3;
+    let lineWidth = (((domainMax / numRings) * 10) / 2) + 50;
     for (var i = 0; i < labels.length; i++) {
       let name = labels[i];
       let angle = (Math.PI / 2) + (2 * Math.PI * i / labels.length);
-      let line_coordinate = getCoordinate(angle, lineWidth);
-      let label_coordinate = getCoordinate(angle, lineWidth + 1);
+      let line_coordinate = getCoordinate(angle, lineWidth, "radial");
+      let label_coordinate = getCoordinate(angle, (lineWidth + 1), "radial");
 
       //draw axis line
       chart.append("line")
       .attr("x1", chart1_cx)
       .attr("y1", chart1_cy)
-      .attr("x2", line_coordinate.x)
-      .attr("y2", line_coordinate.y)
+      .attr("x2", line_coordinate[0])
+      .attr("y2", line_coordinate[1])
       .attr("stroke","black");
 
       //draw axis label
       chart.append("text")
-      .attr("x", label_coordinate.x)
-      .attr("y", label_coordinate.y)
+      .attr("x", label_coordinate[0])
+      .attr("y", label_coordinate[1])
       .text(name);
     }
   }
@@ -136,11 +145,47 @@ d3.csv("Spotify-2000.csv", function (csv) {
       chart1.append("circle")
       .attr("cx", chart1_cx)
       .attr("cy", chart1_cy)
-      .attr("r", radialScale(i*2));
+      .attr("r", scale("radial")(((domainMax / numRings) * i)) / 2);
     }
 
   // Draw Axes on chart1
   drawAxes(attributes, chart1);
+
+  // Plot data
+  var line = d3.line()
+    .x(function(d) {
+      return d[0];
+    })
+    .y(function(d) {
+      return d[1];
+    });
+
+  function getPathCoordinates(data_point){
+    let coordinates = [];
+    for (var i = 1; i < attributes.length; i++){
+        var name = attributes[i];
+        let angle = (Math.PI / 2) + (2 * Math.PI * i / attributes.length);
+        coordinates.push(getCoordinate(angle, data_point[name], data_point.Title));
+    }
+    return coordinates;
+    
+}
+
+  for (var i = 1; i < csv.length; i ++){
+    let d = csv[i];
+    let coordinates = getPathCoordinates(csv[i]);
+
+    //draw the path element
+    chart1.append("path")
+    .datum(coordinates)
+    .attr("d", line)
+    .attr("stroke-width", 3)
+    .attr("stroke", "green")
+    .attr("fill", "green")
+    .attr("stroke-opacity", 1)
+    .attr("opacity", 0.2);
+  }
+
 
 });
 
